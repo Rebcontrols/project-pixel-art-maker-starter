331486460e7f6776d67ecd7f9920f0e790d70657
Rebcontrols <119378283+Rebcontrols@users.noreply.github.com>
2022-12-02 00:31:18 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,8 +1,3 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
 //The user can create a canvas of any (reasonable) size.
 //The user can color the pixels in the grid.
 //The user can reset the grid to a blank state.
@@ -10,37 +5,38 @@
 //Code is neatly formatted and commented.
 //Code runs without errors in the JavaScript console.
 
-const sizePicker = document.querySelector(".sizePicker");
-const canvas = document.getElementById("pixelCanvas");
-const gridBtn = document.getElementById("submit");
-
+let size = document.querySelector(".sizePicker");
+let canvas = document.getElementById("pixelCanvas");
 
 // Select size input
-// When size is submitted by the user, call makeGrid()
-function makeGrid(row, column) {
-    sizePicker.addEventListener('submit', function (canvas){
+sizePicker.addEventListener('click', function (makeGrid){
         // 1. We get the reference where we should place the table
         event.preventDefault();
         // 2. We get the sizes of our inputs
-        let gridHeight = document.querySelector("inputHeight").value();
-        let gridWidth = document.querySelector("#inputWidth").value();
+        let gridHeight = document.querySelector("inputHeight");
+        let gridWidth = document.querySelector("inputWidth");
         // 3. We create a new empty table on the reference we get on step 1 
-        makeGrid(gridheight, gridWidth);
+        makeGrid(gridHeight, gridWidth);
+
+// When size is submitted by the user, call makeGrid()
+function makeGrid(gridHeight, gridWidth) {
 
     // 4. We do a loop to create a new <tr> (row) for each number on our height input
-    for (var x = 1; x <= row - 1; x++) {
-        let gridRow = document.createElement("tr");
-	pixelCanvas.append(gridRow);
+    for (let x = 1; x <= gridHeight - 1; x++) {
+        const gridRow = document.createElement("tr");
+	    gridcanvas.append(gridRow);
         // 5. We do a loop, inside the loop of step 4, to create a new column <td> for each number on the input width
-        for (var y = 0; y <= column - 1; y++) {  
-        let gridCell = document.createElement("td");
-	gridCell.append(gridCell);  
+        for (let y = 0; y <= gridWidth - 1; y++) {  
+        const gridCell = document.createElement("td");
+	    gridCell.append(gridCell);  
         }
     }
     // Select color input
-function() {
-    let color = document.querySelector("color-picker").value();
-        this.style.backgroundColor = color;
-        }
-     })
+    function addColor (event) {
+        var color = document.getElementById('colorPicker').value;
+        event.target.style.backgroundColor = color;
+        event.preventDefault();
+     };
+     grid.addEventListener("click", addColor);
 }
+});
