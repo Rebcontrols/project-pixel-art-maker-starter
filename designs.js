55df3074ae68dad36fba914417314a364687fb62
Rebcontrols <119378283+Rebcontrols@users.noreply.github.com>
2022-12-05 01:44:59 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,42 +1,35 @@
-//The user can create a canvas of any (reasonable) size.
-//The user can color the pixels in the grid.
-//The user can reset the grid to a blank state.
-//The script.js JavaScript code works with the provided HTML and CSS files.
-//Code is neatly formatted and commented.
-//Code runs without errors in the JavaScript console.
+let size = document.querySelector('sizePicker');
+let changeColor = document.getElementById('#colorPicker');
+let gridHeight = document.getElementById('#inputHeight');
+let gridWidth = document.getElementById('#inputWidth');
+let canvas = document.getElementById('#pixelCanvas');
 
-let size = document.querySelector(".sizePicker");
-let canvas = document.getElementById("pixelCanvas");
-
-// Select size input
-sizePicker.addEventListener('click', function (makeGrid){
-        // 1. We get the reference where we should place the table
-        event.preventDefault();
-        // 2. We get the sizes of our inputs
-        let gridHeight = document.querySelector("inputHeight");
-        let gridWidth = document.querySelector("inputWidth");
-        // 3. We create a new empty table on the reference we get on step 1 
-        makeGrid(gridHeight, gridWidth);
-
-// When size is submitted by the user, call makeGrid()
+ 
+//Make the Grid
 function makeGrid(gridHeight, gridWidth) {
-
-    // 4. We do a loop to create a new <tr> (row) for each number on our height input
-    for (let x = 1; x <= gridHeight - 1; x++) {
-        const gridRow = document.createElement("tr");
-	    gridcanvas.append(gridRow);
-        // 5. We do a loop, inside the loop of step 4, to create a new column <td> for each number on the input width
-        for (let y = 0; y <= gridWidth - 1; y++) {  
-        const gridCell = document.createElement("td");
-	    gridCell.append(gridCell);  
-        }
-    }
-    // Select color input
-    function addColor (event) {
-        var color = document.getElementById('colorPicker').value;
-        event.target.style.backgroundColor = color;
-        event.preventDefault();
-     };
-     grid.addEventListener("click", addColor);
-}
+// Select size input
+size.addEventListener("submit", function (event){
+    event.preventDefault()
+    makeGrid(gridHeight, gridWidth);
 });
+
+    canvas.innerHTML = "";
+    //Loop through grid
+    for (let x = 0; x <= gridHeight.height; x++){
+        const gridRow = document.createElement('tr');
+        canvas.appendChild('tr');
+        for (let y = 0; y <= gridWidth.width; y++){ 
+            const gridCell = document.createElement('td');
+            canvas.appendChild('td');
+        }
+
+    }
+
+canvas.addEventListener('click', function (event){
+    if (event.target.nodeName ==="td") {
+        event.target.style.backgroundColor = changeColor.color;
+    }
+});
+
+    makeGrid.appendChild(gridHeight, gridWidth);
+}
