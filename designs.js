fd38e20091503f381cd5775d8944cc9876bcb830
Rebcontrols <119378283+Rebcontrols@users.noreply.github.com>
2022-12-07 07:42:06 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,5 +1,4 @@
 let sizePick = document.getElementById('sizePicker');
-let gridColor = document.querySelectorAll('addListener');
 let height = document.getElementById('inputHeight');
 let width = document.getElementById('inputWidth');
 let canvas = document.getElementById('pixelCanvas');
@@ -9,29 +8,22 @@ function makeGrid(height, width)
     for (let x = 0; x < height.value; x++)
     {
         const gridRow = document.createElement('tr');
-        canvas.appendChild(gridRow); 
+         
         for (let y = 0; y < width.value; y++)
         { 
             const gridCell = document.createElement('td');
-            canvas.appendChild(gridCell);
+            gridRow.appendChild(gridCell);
          }
+         canvas.appendChild(gridRow);
      }
 }
 sizePick.addEventListener('submit', function (event)
 {
     makeGrid(height, width)
     event.preventDefault();
-})
-
-//makeGrid(height, width)
-
-function addListener()
-{
-    for(var cell of cells)
+    canvas.addEventListener('click', function(event)
     {
-        cell.addEventListener('click', c=>
-        {
-            c.target.style.backgroundColor
-        });
-     }
-}
+        const colorPicker = document.getElementById('colorPicker');
+        event.target.style.backgroundColor = colorPicker.value
+    })
+})
